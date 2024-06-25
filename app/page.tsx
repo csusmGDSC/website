72f989e65c6d419c36a1d01920f73f59a9ac30ea
JaedonSpurlock01 <112233517+JaedonSpurlock01@users.noreply.github.com>
2024-06-24 20:57:45 -0700

    Feat: Created a leaderboard.

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Socials from "@/components/main/homepage/socials/socials";
 import Branches from "@/components/main/homepage/branches/branches";
 import NearbyEvents from "@/components/main/homepage/nearby-events/nearby-events";
 import AboutCSUSM from "@/components/main/homepage/about-csusm/about-csusm";
+import Leaderboard from "@/components/main/leaderboard";
 
 /**
  * The home page of the website, for more information on NextJS page routing, visit the documentation
@@ -16,6 +17,7 @@ export default function Home() {
       <NearbyEvents />
       <Benefits />
       <Branches />
+      <Leaderboard />
       <AboutCSUSM />
       <Team />
       <Socials />
