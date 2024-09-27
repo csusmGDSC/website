8e0eb0325cfe72e42bcc38c1368e04f7254b0c05
jaedonspurlock01 <jaedonaspurlock@outlook.com>
2024-09-26 18:59:48 -0700

    test: Created file structure for all tests

--- __tests__/components/faq.tsx
+++ __tests__/components/faq.tsx
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
-import FAQ from "../components/main/resources/faq";
+import FAQ from "../../components/main/resources/faq";
  
-describe('Page', () => {
-  it('renders a heading', () => {
+describe('FAQ component', () => {
+  it('renders the FAQ component and checks if all the questions exist', () => {
     render(<FAQ />)
     const textExists = screen.getByText("Q: Who can join GDSC?")
     expect(textExists).toBeInTheDocument()
